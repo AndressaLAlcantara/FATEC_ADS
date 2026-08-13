83b073a19bd24684b84ebcafc903db5af023d040
Andressa Lima de Alcantara <andressalima8282@gmail.com>
2026-08-13 15:12:04 -0300

    Add files via upload

---
+++ QUANTIDADE_ALIMENTO.py
@@ -0,0 +1,11 @@
+#Declaração de Variáveis
+
+qt: float = 0
+dias: float = 0
+
+#Início
+qt = float(input("Digite a quantidade de alimento disponível="))
+dias = qt/0.05
+print("Esse alimento durará essa quantidade de dias=", dias)
+
+#Fim
